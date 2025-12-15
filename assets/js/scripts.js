8ad9cd16de227d9925946e21e0b633cb1d57bb00
Mosharaf Hossain <team1@Mosharafs-Mac-mini.local>
2025-12-15 14:20:29 +0600

    now create a design for html css and now i style timeline section i done

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,7 +1,13 @@
-   $(document).ready(function() {
-        // Your jQuery code goes here
-        $('.menu-trigger').on(click,function() {
-            $('.hamburger-menu').addClass('isOpen');
-        });
+//    $(document).ready(function() {
+//         // Your jQuery code goes here
+//         $('.menu-trigger').on(click,function() {
+//             $('.hamburger-menu').addClass('isOpen');
+//         });
         
-    });
+//     });
+
+$('.menu-overlay').on('click', function () {
+    $('.hamburger-menu').removeClass('isOpen');
+    $('.menu-overlay').removeClass('active');
+    $('.menu-trigger').removeClass('active');
+});
