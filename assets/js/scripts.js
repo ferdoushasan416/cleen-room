ef74adf5435abdf6c3a4ecd54908b6b992674c74
Mosharaf Hossain <team1@Mosharafs-Mac-mini.local>
2025-12-15 19:05:58 +0600

    now a style and section slick slider

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,13 +1,26 @@
-//    $(document).ready(function() {
-//         // Your jQuery code goes here
-//         $('.menu-trigger').on(click,function() {
-//             $('.hamburger-menu').addClass('isOpen');
-//         });
-        
-//     });
+
+$('.menu-trigger').on('click', function () {
+    $(this).toggleClass('active');
+
+    if ($('.hamburger-menu').hasClass('isOpen')) {
+        $('.hamburger-menu').removeClass('isOpen');
+        $('.menu-overlay').removeClass('active');
+    } else {
+        $('.hamburger-menu').addClass('isOpen');
+        $('.menu-overlay').addClass('active');
+    }
+});
 
 $('.menu-overlay').on('click', function () {
     $('.hamburger-menu').removeClass('isOpen');
     $('.menu-overlay').removeClass('active');
     $('.menu-trigger').removeClass('active');
 });
+
+$('.our-slider-active').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: false,
+  autoplaySpeed: 2000,
+});
+		
